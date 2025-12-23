a3dcc9ddb8b1eb5487067c32cc1557e748c6e34b
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-12-23 17:56:03 -0500

    Refactor main function to properly handle user input and generate content

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
 
     if not response.usage_metadata:
@@ -28,7 +33,6 @@ def main():
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
     print("Gemini Response:")
     print(response.text)
 
